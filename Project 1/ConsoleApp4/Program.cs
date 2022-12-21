408a0f9a4e9a9b4a40ba2b7808303c96f77fe0b6
alahlythebest <121184555+alahlythebest@users.noreply.github.com>
2022-12-21 19:32:09 +0200

    Add files via upload

---
+++ Project 1/ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" The Value n1:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" The Value n2:");
+            int n2 = int.Parse(Console.ReadLine());
+            int num, z, x;
+            num = n1;
+            while (num <= n2)
+            {
+                x = 0;
+                for (z = 2; z < num; z++)
+                {
+
+                    if (num % z == 0)
+                    {
+                        x = 1;
+                            break;
+                    }
+
+                }
+                if (x == 0)
+                    Console.WriteLine(num);
+                num++;
+            }
+            return;
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Project 2/Project 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, z, sum, mx, mz;
+            Console.WriteLine(" Enter The Number 1 Range : ");
+            mx = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Enter The Number 2 Range : ");
+            mz = int.Parse(Console.ReadLine());
+            for (x = mx; x <= mz; x++)
+            {
+                z = 1;
+                sum = 0;
+                while (z < x)
+                {
+                    if (x % z == 0)
+                        sum += z;
+                    z++;
+                }
+                if (sum == x)
+
+                    Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
